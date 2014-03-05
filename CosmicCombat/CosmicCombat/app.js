65c467b91f761c2b85c1ed92d65501caf349132b
Brandon Edgren <brandon.edgren@gmail.com>
2014-03-05 10:47:52 -0800

    Add jquery and reset.min.css

--- CosmicCombat/CosmicCombat/app.js
+++ CosmicCombat/CosmicCombat/app.js
@@ -1,10 +1,8 @@
 ///<reference path="./jquery.d.ts"/>
-
 //class Greeter {
 //    element: HTMLElement;
 //    span: HTMLElement;
 //    timerToken: number;
-
 //    constructor(element: HTMLElement) {
 //        this.element = element;
 //        this.element.innerHTML += "The time is: ";
@@ -12,23 +10,19 @@
 //        this.element.appendChild(this.span);
 //        this.span.innerText = new Date().toUTCString();
 //    }
-
 //    start() {
 //        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
 //    }
-
 //    stop() {
 //        clearTimeout(this.timerToken);
 //    }
-
 //}
-
 //window.onload = () => {
 //    var el = document.getElementById('content');
 //    var greeter = new Greeter(el);
 //    greeter.start();
 //};
-
-window.onload = () => {
+window.onload = function () {
     console.log('hello');
-}
+};
+//# sourceMappingURL=app.js.map
